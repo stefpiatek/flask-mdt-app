4387a8a38664abe86f0ff9d531ab3ba937f9adf7
stef <stefan.piatek@nhs.net>
2017-10-13 11:59:06 +0100

    Add Unit tests for views

--- tests/unit/test_main_views.py
+++ tests/unit/test_main_views.py
@@ -13,18 +13,26 @@ class TestIndex:
 
 @pytest.mark.usefixtures('client_class')
 class TestCaseCreate:
+    def test_setup(self, db_session):
+        patient1 = Patient(id=1, hospital_number=12345678,
+                           first_name='test1', last_name='patient',
+                           date_of_birth='1988-10-09', sex='F')
+        user = User()
+        consultant = User()
+        meeting = Meeting()
 
-    def setup(self):
-        self.patient1 = Patient(id=1, hospital_number=12345678,
-                                first_name='test1', last_name='patient',
-                                date_of_birth='1988-10-09', sex='F')
-
-    def test_page_load(self, db_session):
-        db_session.add(self.patient1)
+        db_session.add(patient1)
         db_session.commit()
+
+    def test_page_load(self):
+
         req_pass = self.client.get(url_for('main.case_create', patient_id=1))
         req_no_id = self.client.get(url_for('main.case_create', patient_id=''))
 
         assert req_pass.status_code == 200
 
         assert req_no_id.status_code == 404, 'no id, page not found'
+
+    def test_kept_in_db(self):
+        req_pass = self.client.get(url_for('main.case_create', patient_id=1))
+        assert req_pass.status_code == 200
